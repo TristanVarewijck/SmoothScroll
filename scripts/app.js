05c4b60bd3f5cb30100fcaf61b4c3ba500e5412d
TristanVarewijck <tristanconnect4@outlook.com>
2022-03-10 12:22:55 +0100

    layout + background images fixed

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,11 @@
 const scroll = new LocomotiveScroll({
   el: document.querySelector("[data-scroll-container]"),
   smooth: true,
-  direction: "horizontal",
+  direction: "horizontal", // if target is met change the direction to vertical
   lerp: 0.03,
 });
+
+// 1. target
+// 2. horzintal
+// 3. target
+// 4. vertical
